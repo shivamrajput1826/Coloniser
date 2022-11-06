bf83d7195ffb681a916c95acfd6679f7833c927f
shivam8.singh <shivam8.singh@paytm.com>
2022-11-06 15:54:12 +0530

    initialised change

---
+++ src/views/landing/LandingPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Welcome to Colony</h1>
+  <h2>Please Select the profile</h2>
+  <h3>Work Profile</h3>
+  <img src="@/assets/work.jpg" />
+  <h3>Personal Profile</h3>
+  <img src="@/assets/personal.jpg" />
+</template>
