62c0cabccc0d5595ce476fd00ddf624b96d6da1e
shivam8.singh <shivam8.singh@paytm.com>
2022-11-06 19:23:29 +0530

    changes

--- src/views/landing/LandingPage.vue
+++ src/views/landing/LandingPage.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>Welcome to Colony</h1>
+  <h1 class="mt-10">Welcome to Colony</h1>
+  <p>It provides</p>
   <h2>Please Select the profile</h2>
-  <h3>Work Profile Section</h3>
-  <img src="@/assets/work.jpg" />
-  <h3>Personal Profile</h3>
-  <img src="@/assets/personal.jpg" />
+  <div class="flex justify-around mt-16">
+    <h2>Work Profile</h2>
+    <h3>Personal Profile</h3>
+  </div>
+  <div class="flex justify-around">
+    <img src="@/assets/work.jpg" />
+    <img src="@/assets/personal.jpg" />
+  </div>
 </template>
+
+<style scoped>
+img {
+  max-height: 60vh !important;
+  max-width: 40vh !important;
+  object-fit: cover;
+}
+</style>
